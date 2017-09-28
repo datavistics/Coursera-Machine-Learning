46193d8205c620d01fb52e04a2f165ecc3e290dd
datavistics <datavistics@gmail.com>
2017-09-28 11:02:13 -0500

    Finished ex4!

--- Octave Code/ex4/nnCostFunction.m
+++ Octave Code/ex4/nnCostFunction.m
@@ -63,21 +63,63 @@ Theta2_grad = zeros(size(Theta2));
 %
 
 
+%%%%%%%%%%
+% Part 1 %
+%%%%%%%%%%
 
+a1 = [ones(m, 1) X];
 
+z2 = a1 * Theta1';
+a2 = sigmoid(z2);
 
+% Adding the offset
+a2 = [ones(size(a2, 1),1) a2];
 
+z3 = a2 * Theta2';
+h_theta = sigmoid(z3);
 
+y_vec = zeros(m,num_labels);
 
+for i = 1:m
+    y_vec(i,y(i)) = 1;
+end
 
+J = 1/m * sum(sum(-1 * y_vec .* log(h_theta)-(1-y_vec) .* log(1-h_theta)));
 
+regularization_term = (sum(sum(Theta1(:,2:end).^2)) + sum(sum(Theta2(:,2:end).^2))) * (lambda/(2*m));
+J = J + regularization_term;
+%%%%%%%%%%
+% Part 2 %
+%%%%%%%%%%
 
+for t = 1:m
 
+	% For the input layer, where l=1:
+	a1 = [1; X(t,:)'];
 
+	% For the hidden layers, where l=2:
+	z2 = Theta1 * a1;
+	a2 = [1; sigmoid(z2)];
 
+	z3 = Theta2 * a2;
+	a3 = sigmoid(z3);
 
+	yy = ([1:num_labels]==y(t))';
+	% For the delta values:
+	delta_3 = a3 - yy;
 
+	delta_2 = (Theta2' * delta_3) .* [1; sigmoidGradient(z2)];
+	delta_2 = delta_2(2:end); % Taking of the bias row
 
+	% delta_1 is not calculated because we do not associate error with the input    
+
+	% Big delta update
+	Theta1_grad = Theta1_grad + delta_2 * a1';
+	Theta2_grad = Theta2_grad + delta_3 * a2';
+end
+
+Theta1_grad = (1/m) * Theta1_grad + (lambda/m) * [zeros(size(Theta1, 1), 1) Theta1(:,2:end)];
+Theta2_grad = (1/m) * Theta2_grad + (lambda/m) * [zeros(size(Theta2, 1), 1) Theta2(:,2:end)];
 
 
 % -------------------------------------------------------------
